3f48344fc2e7ecdfd162da204fc4849201ce57f3
hvitmid <112760988+hvitmid@users.noreply.github.com>
2022-10-21 13:25:56 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -4,14 +4,35 @@
 void main(void)
 {
 	setlocale(LC_ALL, "RUS");
-	int n;
-	scanf_s("%d", &n);
+	/*int x;
+	scanf_s("%d", &x);
 	for (int start = 1; start <= 10; start++)
 		putchar('*');
+	printf("\n");
 	for (int start = 0; start <= 0; start++);
-		putchar('+');
-	for (int start = 1; start <= n; start++)
+	putchar('+');
+	for (int start = 1; start <= x; start++)
 		putchar('*');
 	for (int start = 0; start <= 0; start++)
 		putchar('+');
+	printf("\n");
+	for (int start = 0; start <= x; start++)
+	{
+		putchar('+');
+		putchar('!');
+	}
+	*/
+	printf("\n\n");
+	puts("Задание 1");
+	int m, n, c;
+	puts("Введите значения");
+	scanf_s("%d", &n);
+	for (int i = m, s = 0; i >= n; i++)
+	{
+		s += i;
+		printf("выполнено %d раз", i + 1);
+		c = s;
+	}
+	printf("результат %d", &c);
+
 }
