e79b128a3e2652baa90d0bd9a50848b14fe86db3
hvitmid <112760988+hvitmid@users.noreply.github.com>
2022-10-21 13:50:29 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,10 +1,12 @@
 #include<locale.h>
 #include<stdio.h>
+#include<math.h>
 
 void main(void)
 {
 	setlocale(LC_ALL, "RUS");
 	/*int x;
+	puts("Задание 0");
 	scanf_s("%d", &x);
 	for (int start = 1; start <= 10; start++)
 		putchar('*');
@@ -21,10 +23,10 @@ void main(void)
 		putchar('+');
 		putchar('!');
 	}
-	*/
+
 	printf("\n\n");
 	puts("Задание 1");
-	int m, n, s,i;
+	int m, n, s, i;
 	puts("Введите значения");
 	scanf_s("%d\n %d", &n,&m);
 	for (i = m, s = 0; i <= n; i++)
@@ -33,5 +35,18 @@ void main(void)
 		printf("выполнено %d раз\n", i + 1);
 	}
 	printf("результат %d\n", s);
+	*/
 
+	puts("Задание 2");
+	int x, t, f, i;
+	puts("Введите значения табуляции");
+	scanf("%d\n", &t);
+
+	for (i = 0; i <= t; i++)
+	{
+		puts("\nВведите аргумент");
+		scanf("%d\n", &x);
+		f = pow(x, 3) + 3 * pow(x, 2) - 3;
+		printf("| %d |   %d |", x, f);
+	}
 }
